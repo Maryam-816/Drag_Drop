8658e6d4d4d9f84d874db4e905cd8e8b4a20e681
Maryam-816 <rustamova.maryam2009@gmail.com>
2020-07-18 09:51:23 +0400

    script work started

---
+++ main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let
+        box1 = document.getElementById("box1");
+        box2 = document.getElementById("box2");
+        drag_img = document.getElementById("logo");
+
+    });
