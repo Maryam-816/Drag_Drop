f605901728ff4093471074d22b83299729b1370f
Maryam-816 <rustamova.maryam2009@gmail.com>
2020-07-18 10:03:02 +0400

    dragstart completed

--- main.js
+++ main.js
@@ -4,4 +4,11 @@ document.addEventListener("DOMContentLoaded", function () {
         box2 = document.getElementById("box2");
         drag_img = document.getElementById("logo");
 
+                //dragstart
+
+                drag_img.addEventListener("dragstart", function (event) {
+                    //drag(event);
+                e.dataTransfer.setData("image", event.target.id);
+                });
+
     });
